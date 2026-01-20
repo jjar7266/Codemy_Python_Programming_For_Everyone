ad0b0c2d6a67ad552392eb1fb788a14926605125
jjar7266 <jjar7266@gmail.com>
2026-01-20 15:25:28 -0500

    added examples of string manipulation

---
+++ string_manipulation.py
@@ -0,0 +1,43 @@
+# Codemy.com
+
+# Course: Python Programming For Everyone
+
+# Instructor: John Elder
+
+# string_manipulation.py
+
+# Code written by Jose "Joe" Ruiz
+
+# Modified instructors code to be more Pythonic and readable
+
+# Import module
+
+import os
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+clear_screen()
+
+name = "my name is Bad MoFo"
+
+# .upper()
+# .lower()
+# .capitalize()
+# .title()
+# .swapcase()
+# len() - length
+
+
+# uncomment to print
+
+# print(name.upper())  # prints in all uppercase
+# print(name.lower())
+# print(name.capitalize())
+# print(name.title())
+# print(name.swapcase())
+# print(len(name))  # what's the length of name
+
+print(name[11:])  # index slicing (starts at 0)
+
+print(name.split(" ")[3])  # splits using the space character
